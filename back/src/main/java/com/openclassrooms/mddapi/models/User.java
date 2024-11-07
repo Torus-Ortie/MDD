3e97beb0862fcfe0d285b97a05dc8efe49ba799d
Torus-Ortie <velez.romain@orange.fr>
2024-11-07 15:38:33 +0100

    Implement data

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/User.java
@@ -0,0 +1,45 @@
+package com.openclassrooms.mddapi.models;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "comments")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<Long> subscribedThemeIds = new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_at;
+}
---
+++ back/src/main/java/com/openclassrooms/mddapi/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.openclassrooms.mddapi.repositories;
+
+import com.openclassrooms.mddapi.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @NonNull
+    Optional<User> findById(@NonNull Long id);
+
+    User findByEmail(String email);
+    User findByUsername(String username);
+}
