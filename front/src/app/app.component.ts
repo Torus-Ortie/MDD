0508581edb5dab9c668182df03e419f966435b34
Torus-Ortie <velez.romain@orange.fr>
2024-11-08 15:18:55 +0100

    Implementation of login and register route

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
+import { User } from './interfaces/user.interface';
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,35 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MDD';
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private sessionService: SessionService) {
+  }
+
+  public ngOnInit(): void {
+    this.autoLog();
+  }
+
+  public $isLogged(): Observable<boolean> {
+    return this.sessionService.$isLogged();
+  }
+
+  public logout(): void {
+    this.sessionService.logOut();
+    this.router.navigate([''])
+  }
+
+  public autoLog(): void {
+    this.authService.me().subscribe(
+      (user: User) => {
+        this.sessionService.logIn(user);
+      },
+      (_) => {
+        this.sessionService.logOut();
+      }
+    )
+  }
 }
